d162648b165249d50d1f75893d4551ad8d5efba1
kenes <kenesvip@gmail.com>
2023-09-19 16:31:18 +0500

    Module #4.2 Task #4 Done

--- src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
+++ src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
@@ -2,6 +2,26 @@ package school.mjc.stage0.loops.task4;
 
 public class NumberToBreakOn {
     public void printNumbersUntilWithBreakOn(int toBreakWith, int numberToGoUntil) {
+        if (toBreakWith > numberToGoUntil){
+            System.out.println("iterating till the end");
+            iterator(numberToGoUntil, numberToGoUntil);
+            return;
+        }
+        iterator(toBreakWith, numberToGoUntil);
+    }
 
+    private void iterator(int toBreakWith, int numberToGoUntil) {
+        for (int i = 1; i <= numberToGoUntil; i++){
+            System.out.println(i);
+            if (i == toBreakWith){
+                break;
+            }
+
+        }
+    }
+
+    public static void main(String[] args) {
+        NumberToBreakOn n = new NumberToBreakOn();
+        n.printNumbersUntilWithBreakOn(5, 10);
     }
 }
--- src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
+++ src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
@@ -2,6 +2,25 @@ package school.mjc.stage0.loops.task4;
 
 public class TwoRangesSum {
     public void printSumOfTwoRanges(int numberToSkip, int lastInRow) {
+        if (numberToSkip > lastInRow){
+            System.out.println("number to skip is bigger then the last");
+            return;
+        } else if (lastInRow < 0){
+            System.out.println("last number in row is negative");
+            return;
+        }
 
+        int numToSkip = printedSum(1, numberToSkip);
+        int sum = printedSum(numberToSkip + 1, lastInRow);
+        System.out.println("skipped sum is "+numToSkip);
+        System.out.println("counted sum is "+sum);
+    }
+
+    private int printedSum(int start, int end) {
+        int sum = 0;
+        for (int i = start; i <= end; i++){
+            sum += i;
+        }
+        return sum;
     }
 }
